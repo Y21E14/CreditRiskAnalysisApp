b735c552e7ca4aaa489086604aeaa4893b8916a5
CHEW YU EN RP <22012778@myrp.edu.sg>
2024-12-26 05:08:01 +0800

    Add project files.

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace CreditRiskAnalysisApp.Data // Ensure this matches your project's namespace
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        // Add DbSet properties for your models
+        
+        //public DbSet<User> Users { get; set; }
+    }
+}
