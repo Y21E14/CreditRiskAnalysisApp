e481d5564fc3f51b4d4a261ca7e51b671db574bf
CHEW YU EN RP <22012778@myrp.edu.sg>
2025-01-17 12:32:28 +0800

    Fields added for analysis form

--- Models/AnalysisInput.cs
+++ Models/AnalysisInput.cs
@@ -2,11 +2,20 @@
 {
     public class AnalysisInput
     {
-        public string? CompanyName { get; set; }
-        public int CreditScore { get; set; }
-
-        public int Id { get; set; } // Primary Key
-        public string InputName { get; set; }
-        public string Description { get; set; }
+        public decimal TotalAsset { get; set; }
+        public decimal Cash { get; set; }
+        public decimal TotalDebtInCurrentLiabilities { get; set; }
+        public decimal TotalLongTermDebt { get; set; }
+        public decimal? EarningsBeforeInterest { get; set; }
+        public decimal GrossProfitLoss { get; set; }
+        public decimal TotalLiabilities { get; set; }
+        public decimal RetainedEarnings { get; set; }
+        public decimal TotalStockholdersEquity { get; set; }
+        public decimal TotalInterestAndRelatedExpense { get; set; }
+        public decimal? TotalMarketValue { get; set; }
+        public decimal TotalInventories { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public decimal OperatingActivitiesNetCashFlow { get; set; }
+        public decimal FinancingActivitiesNetCashFlow { get; set; }
     }
 }
