5a505b5b7bde001bcbaf51e468c60a4676038afe
CHEW YU EN RP <22012778@myrp.edu.sg>
2025-01-23 02:08:48 +0800

    Field validation

--- Models/AnalysisInput.cs
+++ Models/AnalysisInput.cs
@@ -1,7 +1,6 @@
 ﻿namespace CreditRiskAnalysisApp.Models;
 using System.ComponentModel.DataAnnotations;
 
-{
     public class AnalysisInput
     {
         [Required(ErrorMessage = "Total Asset is required")]
@@ -12,18 +11,46 @@ using System.ComponentModel.DataAnnotations;
         [Range(0.01, double.MaxValue, ErrorMessage = "Cash must be a positive number")]
         public decimal Cash { get; set; }
 
+        [Required(ErrorMessage = "Total Debt in Current Liabilities is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Total Debt in Current Liabilities must be a positive number")]
         public decimal TotalDebtInCurrentLiabilities { get; set; }
+
+        [Required(ErrorMessage = "Total Long Term Debt is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Total Long Term Debt must be a positive number")]
         public decimal TotalLongTermDebt { get; set; }
+
+        [Required(ErrorMessage = "Earnings before Interest is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Earnings before Interest must be a positive number")]
         public decimal? EarningsBeforeInterest { get; set; }
+    
         public decimal GrossProfitLoss { get; set; }
+
+        [Required(ErrorMessage = "Total Liabilities is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Total Liabilities must be a positive number")]
         public decimal TotalLiabilities { get; set; }
+
         public decimal RetainedEarnings { get; set; }
+
+        [Required(ErrorMessage = "Total Stockholders Equity is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Total Stockholders Equity must be a positive number")]
         public decimal TotalStockholdersEquity { get; set; }
+
         public decimal TotalInterestAndRelatedExpense { get; set; }
+
+        [Required(ErrorMessage = "Total Market Value is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Total Market Value must be a positive number")]
         public decimal? TotalMarketValue { get; set; }
+
+        [Required(ErrorMessage = "Total Inventories is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Total Inventories must be a positive number")]
         public decimal TotalInventories { get; set; }
+
+        [Required(ErrorMessage = "Total Revenue is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Total Revenue must be a positive number")]
         public decimal TotalRevenue { get; set; }
+
         public decimal OperatingActivitiesNetCashFlow { get; set; }
+
         public decimal FinancingActivitiesNetCashFlow { get; set; }
     }
-}
+    
